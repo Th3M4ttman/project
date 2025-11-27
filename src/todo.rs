b6a21536ddad3edb4da99ca94fd86e7eb930f5f8
Th3M4ttman <workmatthewjharris94@gmail.com>
2025-11-27 22:32:13 +0000

    .

--- src/todo.rs
+++ src/todo.rs
@@ -1,6 +1,17 @@
+mod utils
+
+
 use anyhow::Result;
+use std::fs;
+
+use std::path::{Path, PathBuf};
+
+
 
 pub fn todo_list() -> Result<()> {
+    let proj_file = Path::new("~/.config/project/");
+    let data = utils::read_json(proj_file);
+
     println!("List todos");
     Ok(())
 }
