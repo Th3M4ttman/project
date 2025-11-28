c479b421ccd21954c743c0223a6ce7dcf2d6c287
Th3M4ttman <workmatthewjharris94@gmail.com>
2025-11-28 00:07:16 +0000

    feat: Implement todo list functionality and refactor data handling
    Introduces the core functionality for the todo list feature by:
    - Adding  directly to  for local data handling.
    - Refactoring  to correctly locate and create the configuration directory () if it doesn't exist.
    - Utilizing  to parse and display todo items from .
    - Removing dependency on  module for JSON reading.

--- src/todo.rs
+++ src/todo.rs
@@ -1,16 +1,32 @@
-mod utils
 
 
 use anyhow::Result;
 use std::fs;
-
+use serde_json::{Value, json};
 use std::path::{Path, PathBuf};
 
-
+pub fn read_json(path: &Path) -> Value {
+    if let Ok(content) = fs::read_to_string(path) {
+        serde_json::from_str(&content).unwrap_or(json!({}))
+    } else {
+        json!({})
+    }
+}
 
 pub fn todo_list() -> Result<()> {
-    let proj_file = Path::new("~/.config/project/");
-    let data = utils::read_json(proj_file);
+    let project_config = dirs::home_dir().unwrap().join(".config/project/");
+    let todos_file = project_config.join("todos.json");
+    let proj_file = Path::new(&todos_file);
+    
+    if !project_config.exists(){
+        fs::create_dir_all(&project_config)?;
+    }
+
+    let data = read_json(proj_file);
+    match data.get("todos") {
+        Some(val) => println!("{}", val),
+        None => eprintln!("Key not found."),
+    }
 
     println!("List todos");
     Ok(())
@@ -24,4 +40,4 @@ pub fn todo_add(text: &str) -> Result<()> {
 pub fn todo_remove(pattern: &str) -> Result<()> {
     println!("Remove todo: {}", pattern);
     Ok(())
-}
+} 
